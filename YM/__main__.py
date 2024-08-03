3f6a8319c319f5b25a5d610ad42ed69603028e2f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-03 14:40:34 +0530

    Update __main__.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- YM/__main__.py
+++ YM/__main__.py
@@ -40,4 +40,4 @@ async def start_bot():
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.create_task(start_bot())
-    uvicorn.run(sio_app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(sio_app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)), workers=1)
